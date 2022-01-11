8e72cd5fd8aaeb072c1395cee7860b796df3a306
Victor <victordelimas@gmail.com>
2022-01-10 22:23:44 -0300

    feat: create first components

---
+++ src/pages/Home.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Platform,
+} from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
+
+export function Home() {
+    const [newSkill, setNewSkill] = useState('');
+    const [mySkills, setMySkills] = useState([]);
+
+    function handleAddNewSkill(){
+        setMySkills(oldState => [...oldState, newSkill]);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Welcome Victor
+            </Text>
+            <TextInput
+                style={styles.input}
+                placeholder='New skill'
+                placeholderTextColor={"#555"}
+                onChangeText={setNewSkill}
+            />
+            <Button onPress={handleAddNewSkill}/>
+
+            <Text style={[styles.title, { marginVertical: 50 }]}>
+                My Skills
+            </Text>
+         {
+             mySkills.map((skill, index) => (
+                <SkillCard skill={skill} index={index}/>
+             ))
+        }
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#121015',
+        flex: 1,
+        paddingHorizontal: 30,
+        paddingVertical: 70
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+})
